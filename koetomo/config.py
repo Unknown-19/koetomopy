43b8ef417bddbd09bc218b2144925f52752cc222
Unknown <56164909+Unknown-19@users.noreply.github.com>
2021-07-14 22:20:08 +0900

    Create config.py

---
+++ koetomo/config.py
@@ -0,0 +1,8 @@
+class config:
+    HOST_URL = "https://api.meetscom.com"
+    USER_AGENT = "okhttp/3.12.1"
+    EMAIL_REGISTER = "/api/account/entered_email"
+    ID_REGISTER = "/api/account/registration_id"
+    VERSION = "android_3.5.23"
+    SIGNUP_ACCOUNT = "/api/account/signup"
+    LOGIN_ACCOUNT = "/api/account/login"
